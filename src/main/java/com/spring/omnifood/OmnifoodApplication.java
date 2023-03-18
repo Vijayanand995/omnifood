56fc1851d9773b4b606ab280b40c70c75e125a44
Vijayanand995 <vijay.anand2012@gmail.com>
2023-03-18 18:20:05 +0000

    Initial commit

---
+++ src/main/java/com/spring/omnifood/OmnifoodApplication.java
@@ -0,0 +1,13 @@
+package com.spring.omnifood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmnifoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmnifoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/omnifood/OmnifoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.omnifood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmnifoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
