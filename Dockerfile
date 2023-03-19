e28a019f036536755486b0e062c42a306a65bc14
Vijayanand995 <vijay.anand2012@gmail.com>
2023-03-19 15:32:34 +0000

    New Changes

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:8
 EXPOSE 8080
-ADD out/artifacts/omnifood_jar/omnifood.jar omnifood.jar
+ADD target/omnifood.jar omnifood.jar
 ENTRYPOINT ["java","-jar","/omnifood.jar"]
--- src/main/java/com/spring/omnifood/OmnifoodApplication.java
+++ src/main/java/com/spring/omnifood/OmnifoodApplication.java
@@ -2,9 +2,13 @@ package com.spring.omnifood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class OmnifoodApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(OmnifoodApplication.class, args);
